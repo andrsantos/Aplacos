571df610d8151808d303068eaeaed09b42713ecc
201711140047 <201711140047@LAB1-PC06.facomp.ufpa.com>
2017-10-30 20:06:57 -0300

    APrendendo Laços
    ApLaços

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using std::cout;
+using std::cin;
 
-int main(int argc, char **argv)
+int main(int argc, char**argv)
 {
-	printf("hello world\n");
-	return 0;
+    int count = 0;
+    int nValor;
+    double Valor;
+    double soma;
+    
+    while(count <10)
+    {
+        cout << "O valor após o incremente é:" << count << 'n';
+        count++;
+        
+    }
+    
+cout <<"\n\n\Digite o numero de valores a serem somados:\n";
+cin >> nValor;
+count = 0;
+soma = 0;
+while(count<nValor)
+{
+     cout << "Digite um valor:" << '\n';
+     cin >> Valor;
+}
 }
